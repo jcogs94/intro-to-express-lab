b190805bb00e554655c298008d6f1286878c5911
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-04-19 14:49:26 -0400

    ex 3

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
-const validator = require('validator');
 const app = express();
+
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
 
 // 1. Be Polite, Greet the User
 // Responds to /greetings/<username>
@@ -12,10 +18,21 @@ app.get('/greetings/:name', (req, res) => {
 // Uses validator npm package to confirm the input is a number
 app.get('/roll/:num', (req, res) => {
     let input = req.params.num;
-    if (validator.isNumeric(input)) {
+    if (Number.isInteger(input)) {
         res.send(`You rolled a ${Math.floor(Math.random() * (Number(input) + 1))}.`);
     } else {
         res.send('You must specify a number.');
+    }
+});
+
+// 3. I Want THAT One!
+// matches routes for the data set given, verifies if it exists
+app.get('/collectibles/:index', (req, res) => {
+    let index = req.params.index;
+    if (collectibles[index] === undefined) {
+        res.send('This item is not yet in stock. Check back soon!');
+    } else {
+        res.send(`So you want the ${collectibles[index].name}? For \$${collectibles[index].price}, it can be yours!`);
     }
 });
 
